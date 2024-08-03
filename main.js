f59ba5451c09ea75dfc86edf717dcfd54a9bb2e5
Abdulrahmaneltohamy <eltohammy23@gmail.com>
2024-08-03 13:02:18 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var typed = new Typed(".typed", {
+    strings: ["Developer", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: true,
+    cursorChar: "|"
+  });
